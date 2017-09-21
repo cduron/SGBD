9dcd76e47e6685a685aa37ce98ce8f0b6559244d
ig09468 <32173303+ig09468@users.noreply.github.com>
2017-09-22 00:07:02 +0200

    Add files via upload

--- ProjetBDD_Michel_Meynadier_Duron/Code/SGBD/sources/split.c
+++ ProjetBDD_Michel_Meynadier_Duron/Code/SGBD/sources/split.c
@@ -4,9 +4,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define NBMOT 15
 
 //#include "split.h"
+
+
+int nbLettreAvantSeparateur(char *separateur, char*phrase, int numeroMot){
+	int cptlettre = 0;
+	while(phrase[cptlettre]!=' '&&'\0'!=phrase[cptlettre]!='\0')
+		cptlettre++;
+	return cptlettre;
+}
+
 
 /*
 * Split 				: permetant de séparer une chaine de caracteres en un tableau de chaines de caracteres.
@@ -15,43 +23,67 @@
 *  return rtnchaine 	: tableau de chaines de chaines de caractères
 * 
 *
-*  var int cptchar		: compte le nombre de caractère
+*  var char* token		: permet de récuperer les mots
 *  var int ligne		: compteur de ligne
-*  var int cptfirst 	: coordonné du de la première lettre d'un mot
 */
-char ** split (char *chaine, char separateur, int nbMot){
-	int cptchar=0, ligne=0, cptfirst=0;
-	char *rtnchaine[nbMot+1];
-	while(chaine[cptchar]=='\0'){
-		printf("1");
-		if(chaine[cptchar]==separateur){
-			/* alloue dynamiquement à la taille exact du mot*/
-			rtnchaine[ligne]=malloc(sizeof(char)*(cptchar+2-cptfirst));
-			/* copie le mot trouver dans rtnchaine en prenant le */
-			strncpy(rtnchaine[ligne], chaine+cptfirst, cptchar-1);
-			if(rtnchaine[ligne]==NULL){
-				printf("Erreur fatal, case %d vide", ligne);
-				return NULL;
-			}else
-			printf("%s\n", rtnchaine[ligne]);
 
-			ligne++;
-			/* cptfirst devient la position de la première lettre du prochain mot */
-
-			cptfirst=1+cptchar;
-			printf("%d\n", cptfirst);
-		}	
-		/* passage au caractère d'après s'il est différent du séparateur */ 
-		cptchar++;
+char **split(char *chaine, char* separateur, int nbcolonne){
+	int ligne=0, boucle=0;
+	char *token;
+	/* Alloue le tableau de retour */
+	char ** rtnchaine=malloc(sizeof(char*)*(nbcolonne+1));
+	if(rtnchaine==NULL)
+	{
+		printf("Erreur rtnchaine NULL\n");
+		free(rtnchaine);
+		return NULL;
+	
 	}
-
+	/*Affecte le premier mot dans token */
+	token=strtok(chaine, separateur);
+	/*Alloue la taille du mot à la ligne 0 */
+	rtnchaine[ligne]=malloc(sizeof(char)*(nbLettreAvantSeparateur(separateur, chaine, ligne)+1));
+	/*Stock le mot dans le tableau */
+	rtnchaine[ligne]=token;
+	ligne++;
+	while(token!=NULL){
+		/* Alloue la taille du mot à la ligne suivante */
+		rtnchaine[ligne]=malloc(sizeof(char)*(nbLettreAvantSeparateur(separateur, chaine, ligne)+1));
+		if(rtnchaine[ligne]==NULL){
+			printf("Erreur rtnchaine[%d] NULL\n", ligne);
+			free(rtnchaine);
+			return NULL;
+		}
+		/*Stock le mot dans token */
+		token = strtok(NULL, separateur);
+		/*Stock le token au tableau */
+		if(token!='\0'){
+			printf("%s\n\n", token);
+			rtnchaine[ligne]=token;
+			ligne++;
+		}
+	}
 	return rtnchaine;
 }
 
-int main(){
-	char *chaine = {
-		"coucou je suis fort"
-	};
-	printf("%s\n", split(chaine, ' ', 4));
-	return 0;
+/*
+* Fonction de test();
+*
+*/
+void testsplit(){
+	char chaine[] = "coucou je suis fort";
+	printf("%s\n\n", chaine);
+	printf("Alloue\n\n");
+	char **souschaine = split(chaine, " ", 4);
+	if(souschaine==NULL)
+		printf("ERREUR D'AFFECTATION\n");
+	if(souschaine==NULL){
+		printf("Erreur NULL\n");	
+		free(souschaine);
+	}
+	for (int i = 0; i < 4; ++i)
+	{
+		printf("%s\n", souschaine[i]);
+	}
+	free(souschaine);
 }
