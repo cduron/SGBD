71edff2501ccda1324749a04f6cf473c0f782136
Duron Clemence <ii05586@pc523a1-24.ens.math-info.univ-paris5.fr>
2017-09-27 12:31:31 +0200

    Modification GlobalManager.c

--- ProjetBDD_Michel_Meynadier_Duron/Code/SGBD/sources/GlobalManager.c
+++ ProjetBDD_Michel_Meynadier_Duron/Code/SGBD/sources/GlobalManager.c
@@ -6,5 +6,16 @@
 #include "/headers/GlobalManager.h"
 
 void init (){
+	printf("/**** Bienvenue dans l'interface de notre SGBD ******/ \n");
+	scanf("%s", &saisie);
+
+
+	if (&saisie=="exit"){
+		printf("/*** Goodbye my lover, Goodbye my friend <3 ****/");
+		exit();
+	}
+	printf("");
+
+	printf("");
   
 }
