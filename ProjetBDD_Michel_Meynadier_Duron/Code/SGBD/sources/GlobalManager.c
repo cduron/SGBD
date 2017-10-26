5acbb82c8476907c265398324092dbb985f633f0
KekAuCitron <nathan.michel679@gmail.com>
2017-10-26 12:55:56 +0200

    TP3 partie B marche

--- ProjetBDD_Michel_Meynadier_Duron/Code/SGBD/sources/GlobalManager.c
+++ ProjetBDD_Michel_Meynadier_Duron/Code/SGBD/sources/GlobalManager.c
@@ -10,25 +10,51 @@
 
 
 struct DbDef dbDef1; /* Declaration de DbDef1 de type DbDef */
+struct HepaFile *listHeapFile;
 
-int main(){
-	return 0;
-}
+
 
 
 void init (){
-
 	dbDef1.compteur_relations = 0;
-
+	FILE* fichier = NULL;
+	fichier = fopen("../DB/Catalog.def", "r+");
+	if (fichier != NULL){
+		fread(&dbDef1, sizeof(dbDef1), 1, fichier);
+		fclose(fichier);
+	}
+	else {
+		printf("Impossible d'ouvrir le fichier Catalog.def");
+	}
 };
 
-void CreateRelation(char NomRelation, int NombreColonnes, char TypesDesColonnes){
+void CreateRelation(char nomRelation, int nombreColonnes, char typesDesColonnes){
 	/* Declaration d'une nouvelle relation de type RelDef */
-	struct RelDef RelDef1;
-	RelDef1.Schema.nom = NomRelation;
-	RelDef1.Schema.nombre_colonnes = NombreColonnes;
-	RelDef1.Schema.type_colonnes = TypesDesColonnes;
-	RelDef1.FileId = dbDef1.compteur_relations;
-	dbDef1.liste_relations[dbDef1.compteur_relations] = RelDef1;
+	struct RelDef relDef1;
+	relDef1.Schema.nom = nomRelation;
+	relDef1.Schema.nombre_colonnes = nombreColonnes;
+	relDef1.Schema.type_colonnes = typesDesColonnes;
+	relDef1.FileId = dbDef1.compteur_relations;
+	dbDef1.liste_relations[dbDef1.compteur_relations] = relDef1;
 	dbDef1.compteur_relations ++;
 };
+
+int finish(){
+	    FILE* fichier = NULL;
+	    fichier = fopen("../DB/Catalog.def", "r+");
+	    if (fichier != NULL){
+	    	fwrite(&dbDef1, sizeof(dbDef1), 1, fichier);
+	    	fclose(fichier);
+	    }
+	    else {
+	        printf("Impossible d'ouvrir le fichier Catalog.def");
+	    }
+	    return 0;
+}
+
+int main(){
+	init();
+	finish();
+	return 0;
+}
+
