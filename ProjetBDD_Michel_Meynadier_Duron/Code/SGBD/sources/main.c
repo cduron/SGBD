41e4c9ff5b80c954b28d03854cbecc6dc837e1f2
KekAuCitron <nathan.michel679@gmail.com>
2017-11-30 12:08:14 +0100

    Prout

--- ProjetBDD_Michel_Meynadier_Duron/Code/SGBD/sources/main.c
+++ ProjetBDD_Michel_Meynadier_Duron/Code/SGBD/sources/main.c
@@ -4,6 +4,31 @@
 
 #include "../headers/GlobalManager.h"
 
-int main (){
-	init();
+int main(){
+	initStruct();
+	struct RelSchema k = {"nani", 3, "yamero"};
+	struct RelDef a={k, 6, 5, 4};
+	struct RelDef *b= malloc(sizeof(*b));
+	b = &a;
+	struct DbDef def={b, 3};
+	struct ListeHeapFile *listeHeapfile = malloc(sizeof(*listeHeapfile));
+	struct  HeapFile hf = {b};
+	    if (listeHeapfile == NULL)
+	    {
+	        exit(EXIT_FAILURE);
+	    }
+	    listeHeapfile->nxt = NULL;
+	    listeHeapfile->present = hf;
+	printf("compteur de relations: %d\n", def.compteurRelations);
+	ajouterEnFin(listeHeapfile, hf);
+	printf("-> %d\n", hf.ptrRelDef->FileId );
+	ListeHeapFile *actuel = listeHeapfile;
+	while (actuel->nxt != NULL)
+		{
+			printf("-> %d\n", actuel->present.ptrRelDef->FileId);
+			actuel = actuel->nxt;
+
+		}
+    printf("NULL\n");
+	//createHeader(hf);
 }
