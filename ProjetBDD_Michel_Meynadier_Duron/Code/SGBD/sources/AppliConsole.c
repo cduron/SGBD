b4dfd82cf75b136d6c4e07797c025d61f07c5ae3
cduron <clemence.duron@sfr.fr>
2017-12-06 17:28:39 +0100

    Insert Fonctionnel

--- ProjetBDD_Michel_Meynadier_Duron/Code/SGBD/sources/AppliConsole.c
+++ ProjetBDD_Michel_Meynadier_Duron/Code/SGBD/sources/AppliConsole.c
@@ -15,55 +15,35 @@
  * param commande : commande en console de l'utilisateur
  */
 void traitementConsole(char *commande){
-	char s[1] = " ";
-
+	char *separateur = " ";
 	//Copie de la Chaîne de commande à partir du 2ème mot : soit après l'opérateur (insert,join etc..)
-	char * c2 ;
-	while(c2==NULL){
-		c2=malloc(sizeof(char)*strlen(commande));
-		if(c2==NULL){
-			printf("Erreur d'affectation");
-		}
-	}
-	strcpy(c2,commande);
-	printf("C2 :  %s\n",c2);
-	//int nbLettre = nbLettreAvantSeparateur(s,c2);
-	//printf("Commande :  %s\n",commande);
-	//strcpy(c2,commande+nbLettre);
-	//printf("C2 :  %s\n",c2);
-
-
-
-	/*char *token;
+	char *token;
 	//token prend la valeur du premier mot de la chaîne commande [Opérateur]
-	token = strtok(commande,s);
-
+	token = strtok(commande, separateur);
+	int taille=strlen(token)+1;
 	//Si la commande est un insert
 	if(strcmp(token,"insert")==0){
-		char * c3;
-		while(c3==NULL){
-			c3=malloc(sizeof(char)*strlen(c2));
-			if(c2==NULL){
-				printf("Erreur d'affectation");
-			}
-		}
-
-		//Pour faire un insert, il faut le nom de la relation : 2ème élément de la chaîne commande
-		token = strtok(NULL,s);
+		char *c3;
+		c3=malloc(sizeof(char)*strlen(commande));
+		token = strtok(NULL, separateur);
+		strcpy(c3, (commande+taille+strlen(token)+1));
 		char * nomRelation = token;
-
-		//Puis on fait le insert avec comme liste de Valeurs la suite de la chaîne commande
-
-		fflush(stdout);		strcpy(c3,c2+strlen(token)+1);
+		printf("%s", token);
+		printf("%s", nomRelation);
+		printf("C3 : %s\n", c3);
 		insert(nomRelation,c3);
-		free(c3);
+		printf("insert réussi");
+
 	}
+	//Pour faire un insert, il faut le nom de la relation : 2ème élément de la chaîne commande
+
+
+	//Puis on fait le insert avec comme liste de Valeurs la suite de la chaîne commande
 
 	else {
 		if (strcmp(token,"exit")==0){
 		}
 
-	}*/
-	//free(c2);
+	}
 
 }
