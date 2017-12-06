e6ac220c85d727c066fdd94d5724349684f8d07c
ig09468 <32173303+ig09468@users.noreply.github.com>
2017-12-06 23:17:39 +0100

    Update AppliConsole.c

--- ProjetBDD_Michel_Meynadier_Duron/Code/SGBD/sources/AppliConsole.c
+++ ProjetBDD_Michel_Meynadier_Duron/Code/SGBD/sources/AppliConsole.c
@@ -15,7 +15,6 @@
  * param commande : commande en console de l'utilisateur
  */
 void traitementConsole(char *commande){
-<<<<<<< HEAD
 	char *separateurEspace = " ";
 	//Copie de la Chaîne de commande à partir du 2ème mot : soit après l'opérateur (insert,join etc..)
 	char *token;
@@ -50,17 +49,15 @@ void traitementConsole(char *commande){
 		strcat(adresse, c2);
 		printf("%s\n", adresse);
 		fichier = fopen("../DB/R1.csv", "r");
-	    char chaine[1000] = "";
-		printf("prout\n");
+	  	char chaine[100000] = "";
 		if (fichier != NULL){
-			while (fgets(chaine, 1000, fichier) != NULL)
+			while (fgets(chaine, 100000, fichier) != NULL)
 			{
 				printf("%s", chaineFichier);
 				strcat(chaineFichier,chaine);
 			}
 		}else
 			printf("Fichier non ouvert");
-		printf("HOLLA AMIGO !");
 		char* chaineFichierFinal;
 		int mst = strlen(chaineFichier)+1;
 		printf("%d", mst);
@@ -76,33 +73,6 @@ void traitementConsole(char *commande){
 		for (int i=1; i<strlen(chaineFichier); i++){
 			token2 = strtok(NULL, separateurPointVirgule);
 			strcat(chaineFichierFinal, token2);
-=======
-	char *separateur = " ";
-	//Copie de la Chaîne de commande à partir du 2ème mot : soit après l'opérateur (insert,join etc..)
-	char *token;
-	//token prend la valeur du premier mot de la chaîne commande [Opérateur]
-	token = strtok(commande, separateur);
-	int taille=strlen(token)+1;
-	//Si la commande est un insert
-	if(strcmp(token,"insert")==0){
-		char *c3;
-		c3=malloc(sizeof(char)*strlen(commande));
-		token = strtok(NULL, separateur);
-		strcpy(c3, (commande+taille+strlen(token)+1));
-		char * nomRelation = token;
-		printf("%s", token);
-		printf("%s", nomRelation);
-		printf("C3 : %s\n", c3);
-		insert(nomRelation,c3);
-		printf("insert réussi");
-
-	}
-	//Pour faire un insert, il faut le nom de la relation : 2ème élément de la chaîne commande
-
-
-	//Puis on fait le insert avec comme liste de Valeurs la suite de la chaîne commande
->>>>>>> b53867ba47a10e7a5dd503024e5095482ae128e1
-
 		}
 		insert (nomRelation, chaineFichierFinal);	//on créer le record avec cette chaine
 		free(c2);
@@ -111,12 +81,7 @@ void traitementConsole(char *commande){
         fclose(fichier);
 
 	}
-<<<<<<< HEAD
-
 	else if (strcmp(token,"exit")==0){
 		finish();
 	}
-=======
->>>>>>> b53867ba47a10e7a5dd503024e5095482ae128e1
-
 }
